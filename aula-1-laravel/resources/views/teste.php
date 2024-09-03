782797a5c3e1b769ee5efa8d98ff7ad8591a726e
Lucas Scquiavon <navarrolucas0009@gmail.com>
2024-09-03 13:32:23 -0300

    first

---
+++ aula-1-laravel/resources/views/teste.php
@@ -0,0 +1,5 @@
+<h2>Página de teste Laravel</h2>
+
+<br>
+
+<a href="/">Voltar</a>
---
+++ aula-1-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primeiro projeto laravel</title>
+</head>
+<body>
+    <h1>Pág. incial do projeto.</h1>
+    <br>
+    <a href="/teste">Ir para pág. teste</a>
+</body>
+</html>
